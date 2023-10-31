e145cbcf00e730b9d5a57f4a4cf3c743813ad1dc
Saldivar Martinez Dayana Yamileth <143575706+DSSSqrew1260zxcm@users.noreply.github.com>
2023-10-30 19:40:49 -0600

    Add files via upload

---
+++ eva2_15_while/src/eva2_15_while/Eva2_15_while.java
@@ -0,0 +1,16 @@
+package eva2_15_while;
+
+import java.util.*;
+
+public class Eva2_15_while {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int i = 1;
+        while(i<=10){//Mientras sea verdad la expresión
+            System.out.println("¡¿¡Dayana!?!");
+             i++;
+        }
+    }
+
+}
