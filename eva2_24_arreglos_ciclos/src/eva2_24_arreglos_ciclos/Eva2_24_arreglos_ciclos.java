f42710c13dd4de67ea748f71a2a2f8d750bbfe69
Saldivar Martinez Dayana Yamileth <143575706+DSSSqrew1260zxcm@users.noreply.github.com>
2023-11-08 18:36:46 -0700

    Add files via upload

---
+++ eva2_24_arreglos_ciclos/src/eva2_24_arreglos_ciclos/Eva2_24_arreglos_ciclos.java
@@ -0,0 +1,17 @@
+package eva2_24_arreglos_ciclos;
+
+public class Eva2_24_arreglos_ciclos {
+
+    public static void main(String[] args) {
+        int[] arreglo = new int[10];
+        //primero = 0; ultimo = n-1
+        for (int i = 0; i < 10; i++) {
+            arreglo[i] = 10000;
+        }
+        for (int i = 0; i < 10; i++) {
+            arreglo[i] = 10000;
+            System.out.println(arreglo[i]);
+        }
+    }
+
+}
